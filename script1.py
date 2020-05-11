914526ea487dbf6e0c38258e9acd8210ae1b78d2
tombetthauser <tombetthauser@gmail.com>
2020-05-10 22:28:50 -0700

    Finish GUI

--- script1.py
+++ script1.py
@@ -10,98 +10,164 @@ import webbrowser
 window = Tk()
 
 window.title("Arts Database Explorer 1.0")
-window.geometry('1050x750')
+window.geometry('1050x760')
 window.configure(bg='#dedede')
 window.resizable(width=False, height=False)
 
-padrow0 = Label(window, text="", bg="#dedede", height=1)
-padrow0.grid(row=0, column=0, columnspan=11)
+Label(window, bg="#dedede", height=1).grid(row=0, column=0, columnspan=11)
 
-padcol0 = Label(window, text="", bg="#dedede", width=2)
-padcol0.grid(row=0, column=0, rowspan=15)
+Label(window, bg="#dedede", width=2).grid(row=0, column=0, rowspan=15)
 
-top_label = Label(window, text="Arts Database Explorer 1.0", width=73, fg="black", bg="white", font='Times 25', relief=SUNKEN, anchor=W, padx=20, pady=20, borderwidth=6)
-top_label.grid(row=1, column=1, columnspan=9)
+Label(
+  window, 
+  text="Arts Database Explorer 1.0", 
+  width=73, 
+  fg="black", 
+  bg="white", 
+  font='Times 25', 
+  relief=SUNKEN, 
+  anchor=W, 
+  padx=20, 
+  pady=20, 
+  borderwidth=6
+).grid(row=1, column=1, columnspan=9)
 
-padrow1 = Label(window, text="", bg="#dedede", height=1)
-padrow1.grid(row=2, column=0, columnspan=11)
+Label(window, bg="#dedede", height=1).grid(row=2, column=0, columnspan=11)
 
-subtitle_label = Label(window, text="A desktop explorer for the State of the Arts 2019 survey results.", width=119, fg="black", bg="white", font='Times 16', relief=SUNKEN, anchor=W, padx=20, pady=10, borderwidth=4)
-subtitle_label.grid(row=3, column=1, columnspan=9)
+Label(
+  window, 
+  text="A desktop explorer for the State of the Arts 2019 survey results.", 
+  width=119, 
+  fg="black", 
+  bg="white", 
+  font='Times 16', 
+  relief=SUNKEN, 
+  anchor=W, 
+  padx=20, 
+  pady=10, 
+  borderwidth=4
+).grid(row=3, column=1, columnspan=9)
 
+Label(window, bg="#dedede", height=1).grid(row=4, column=0, columnspan=11)
 
-# style = Style()
-# style.configure('W.TButton', font=('calibri', 10, 'bold', 'underline'),
-#                 foreground='black')
+Button(
+  window, 
+  text='VIEW ALL COLUMNS', 
+  highlightbackground='#777',
+  width=18,
+  anchor=W,
+  cursor='hand2',
+).grid(row='5', column='1')
 
-# close = Button(window, text="CLOSE", width=12, command=window.destroy, highlightbackground="#999", font="Arial 14")
-# close.pack(side=BOTTOM, pady=[20,20], padx=[35,5])
+Button(
+  window, 
+  text='RUN QUERY', 
+  highlightbackground='#64935e',
+  width=18,
+  anchor=W,
+  cursor='hand2',
+).grid(row='5', column='3')
 
+Button(
+  window, 
+  text='CLEAR QUERY', 
+  highlightbackground='#935e5e',
+  width=18,
+  anchor=W,
+  cursor='hand2',
+).grid(row='5', column='5')
 
+Button(
+  window, 
+  text='SAVE RESULTS', 
+  highlightbackground='#777',
+  width=18,
+  anchor=W,
+  cursor='hand2',
+).grid(row='5', column='7')
 
-# Style will be reflected only on
-# this button because we are providing
-# style only on this Button.
-# ''' Button 1'''
-# btn1 = Button(root, text='Quit !',
-#               style='W.TButton',
-#               command=root.destroy)
-# btn1.grid(row=0, column=3, padx=100)
+Button(
+  window, 
+  highlightbackground='#777',
+  command=window.destroy,
+  cursor='hand2',
+  text='CLOSE', 
+  anchor=W,
+  width=18,
+).grid(row='5', column='9')
 
-# ''' Button 2'''
-# btn2 = Button(root, text='Click me !', command=None)
-# btn2.grid(row= 1, column = 3, pady = 10, padx = 100)
+# Label(window, height=1, bg='#dedede').grid(row='6', column='0', columnspan=11)
 
+Label(
+  text="Enter your SQL query here:",
+  font='Times 16',
+  bg='#dedede',
+  pady=15,
+  anchor=W,
+  width=23,
+).grid(row='7', column='1')
 
+query_text = StringVar()
+Entry(window, 
+  textvariable=query_text, 
+  highlightthickness=0,
+  borderwidth=10,
+  relief=FLAT, 
+  width=108, 
+).grid(row=8, column=1, columnspan=9)
 
+Label(
+  text="Below are your query results:",
+  font='Times 16',
+  bg='#dedede',
+  pady=15,
+  anchor=W,
+  width=23,
+).grid(row=10, column=1)
 
+Listbox(window,
+  highlightthickness=0,
+  borderwidth=10,
+  relief=FLAT, 
+  height= 18, 
+  width=108, 
+).grid(row=11, column=1, columnspan=9)
 
+Label(window, bg="#dedede", height=0, pady=1).grid(row=12, column=0, columnspan=11)
 
+def callback(url): webbrowser.open_new(url)
 
+link1 = Button(
+    window,
+    highlightbackground='#bbb',
+    cursor='hand2',
+    text='Visit Survey Page',
+    anchor=W,
+    width=18,
+)
+link1.bind("<Button-1>", lambda e: callback("http://sotasurvey.org/"))
+link1.grid(row=13, column=1)
 
+link2 = Button(
+    window,
+    highlightbackground='#bbb',
+    cursor='hand2',
+    text='GitHub Project Page',
+    anchor=W,
+    width=18,
+)
+link2.bind("<Button-1>", lambda e: callback("https://github.com/tombetthauser/arts_database"))
+link2.grid(row=13, column=3)
 
-
-
-# l1 = Label(window, text="Enter your SQL query here:")
-# l1.grid(row=0, column=0)
-
-# l2 = Label(window, text="Below are your query results:")
-# l2.grid(row=2, column=0)
-
-# query_text = StringVar()
-# e1 = Entry(window, textvariable=query_text, width=80)
-# e1.grid(row=1, column=0)
-
-# result_box = Listbox(window, height=10, width=50)
-# result_box.pack(fill=X)
-# result_box.grid(row=3, column=0, rowspan=6, columnspan=1)
-
-# scroll_bar = Scrollbar(window)
-# scroll_bar.grid(row=3, column=1, rowspan=6)
-
-# result_box.configure(yscrollcommand=scroll_bar.set)
-# scroll_bar.configure(command=result_box.yview)
+link3 = Button(
+    window,
+    highlightbackground='#bbb',
+    cursor='hand2',
+    text='Learn Basic SQL',
+    anchor=W,
+    width=18,
+)
+link3.bind("<Button-1>", lambda e: callback("https://www.w3schools.com/sql/"))
+link3.grid(row=13, column=5)
 
 window.mainloop()
-
-
-
-
-
-
-
-
-### LINKS
-
-# def callback(url):
-#     webbrowser.open_new(url)
-#     ## To open local files:
-#     # webbrowser.open_new(r"file://c:\test\test.csv")
-
-# link1 = Label(window, text="Google Hyperlink", fg="blue", cursor="hand2")
-# link1.pack()
-# link1.bind("<Button-1>", lambda e: callback("http://www.google.com"))
-# link1.grid(row=4, column=0)
-
-# title_style = ttk.Style()
-# title_style.configure("MainTitle", foreground="red", background="black")
