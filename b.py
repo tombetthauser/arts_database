52e6248d9187a180b03e4cc520f4809df00ae39b
tombetthauser <tombetthauser@gmail.com>
2020-05-11 09:50:25 -0700

    Add connect() in backend.py

---
+++ b.py
@@ -0,0 +1,4 @@
+def foo():
+  print("Whassup?")
+
+foo()
---
+++ backend.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+def connect():
+  conn=sqlite3.connect("data.db")
+  cur=conn.cursor()
+  # cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+  conn.commit()
+  conn.close()
+
+connect()
